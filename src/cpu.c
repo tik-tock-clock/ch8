fc1dd8872a916639257352e391a875c7bde83954
overdrive6754@gmail.com <overdrive6754@gmail.com>
2026-03-08 16:57:36 +0000

    Added bones of Chip8 cpu, It's init function and most of the ROM loading.

--- src/cpu.c
+++ src/cpu.c
@@ -1,127 +1,107 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-char* denToHex(int value)
+uint8_t Chip8FontSet[80] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+typedef struct
 {
-    int denary = value;
-    char hexadec[10];
+    uint8_t Vregisters[16];
+    uint8_t memory[4096];
+    uint16_t indexRegister;
+    uint16_t programCounter;
 
-    if (denary == 0)
-    {
-        printf("00 ");
-        return NULL;
+    //Timers
+    uint8_t delayTimer;
+
+    //The stack
+    uint16_t stack[16];
+    uint8_t stackPointer;
+
+    //Graphics and Input
+    uint8_t display[64*32];
+    uint8_t keypad[16];
+}Chip8;
+
+void init(Chip8 *chip)
+{
+    //Reset Program Counter to 0x200
+    chip->programCounter = 0x200;
+
+    //Reset Index and Stack Pointer
+    chip->indexRegister = 0;
+    chip->stackPointer = 0;
+
+    //Clear Memory, Registers, and Stack
+    memset(chip->memory, 0, sizeof(chip->memory));
+    memset(chip->Vregisters, 0, sizeof(chip->Vregisters));
+    memset(chip->stack, 0, sizeof(chip->stack));
+
+    //Clear Display and Keypad
+    memset(chip->display, 0, sizeof(chip->display));
+    memset(chip->keypad, 0, sizeof(chip->keypad));
+
+    //Reset Timer
+    chip->delayTimer = 0;
+
+    //Load Fontset to beginning of memory
+    for (int i=0; i<80; i++) {
+        chip->memory[0x50 + i] = Chip8FontSet[i];
     }
-
-    int remainder = denary % 16;
-
-    //printf("%i ", remainder);
-
-    int byte1 = (denary - remainder)/16;
-
-    switch (byte1)
-    {
-        case 0:
-            hexadec[0] = '0';
-        case 1:
-            hexadec[0] = '1';
-        case 2:
-            hexadec[0] = '2';
-        case 3:
-            hexadec[0] = '3';
-        case 4:
-            hexadec[0] = '4';
-        case 5:
-            hexadec[0] = '5';
-        case 6:
-            hexadec[0] = '6';
-        case 7:
-            hexadec[0] = '7';
-        case 8:
-            hexadec[0] = '8';
-        case 9:
-            hexadec[0] = '9';
-        case 10:
-            hexadec[0] = 'A';
-        case 11:
-            hexadec[0] = 'B';
-        case 12:
-            hexadec[0] = 'C';
-        case 13:
-            hexadec[0] = 'D';
-        case 14:
-            hexadec[0] = 'E';
-        case 15:
-            hexadec[0] = 'F';    
-    }
-
-    switch (remainder)
-    {
-        case 0:
-            hexadec[1] = '0';
-        case 1:
-            hexadec[1] = '1';
-        case 2:
-            hexadec[1] = '2';
-        case 3:
-            hexadec[1] = '3';
-        case 4:
-            hexadec[1] = '4';
-        case 5:
-            hexadec[1] = '5';
-        case 6:
-            hexadec[1] = '6';
-        case 7:
-            hexadec[1] = '7';
-        case 8:
-            hexadec[1] = '8';
-        case 9:
-            hexadec[1] = '9';
-        case 10:
-            hexadec[1] = 'A';
-        case 11:
-            hexadec[1] = 'B';
-        case 12:
-            hexadec[1] = 'C';
-        case 13:
-            hexadec[1] = 'D';
-        case 14:
-            hexadec[1] = 'E';
-        case 15:
-            hexadec[1] = 'F';    
-    }
-
-    printf("%s ", hexadec);
-    return NULL;
 }
 
-int main(int argc, char* argv[])
+int loadRom(const char* romPath)
 {
-    FILE* fptr;
-
-    fptr = fopen("roms/ch8-logo.ch8", "rb");
-
-    if (fptr == NULL) {
-        printf("File not opened!\n");
-    }
-    else{
-        printf("File opened successfully!\n\n");
-
-        fseek(fptr, 0L, SEEK_END);
-
-        long int fileLength = ftell(fptr);
-
-        fseek(fptr, 0L, SEEK_SET);
-
-        for(int i=0; i<fileLength; i++)
-        {
-            int currentValue;
-            fread(&currentValue, 1, 1, fptr);
-            denToHex(currentValue);
-        }
-        printf("\n");
+    //Open the File and check if the File is opened successfully
+    FILE* rom = fopen(romPath, "rb");
+    if (rom == NULL){
+        printf("File not opened successfully!\n");
+        return 0;
     }
 
-    fclose(fptr);
+    printf("File opened successfully\n");
+
+    //Get File size
+    fseek(rom, 0, SEEK_END);
+    long romSize = ftell(rom);
+    rewind(rom);
+
+    //Allocate memory  buffer to ROM
+    //Sets size of buffer to size of a char times by the size of the ROM in chars
+    //Validates buffer aswell
+    char* romBuffer = (char*) malloc(sizeof(char) * romSize);
+    if (romBuffer == NULL){
+        printf("ERROR: Failed to allocate memory for ROM!\n");
+        return 0;
+    }
+
+    //Copy the rom into the buffer
+    size_t data = fread(romBuffer, sizeof(char), (size_t)romSize, rom);
+    if (data != romSize){
+        printf("ERROR: Failed to read ROM!\n");
+        return 0;
+    }
+}
+
+int main(int argc, const char* argv[])
+{
     return 0;
 }
